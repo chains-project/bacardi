86c2db598e5c3003ebf2006937fea8dcf4541e32
Frank Reyes <frankrg@kth.se>
2024-09-03 17:48:24 +0200

    Adding Logo

--- src/main/java/se/kth/Main.java
+++ src/main/java/se/kth/Main.java
@@ -1,7 +1,22 @@
 package se.kth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("   ( (  )__( )__( ) ) ");
+        System.out.println("    |\\   /\\  /\\  /|");
+        System.out.println("    | \\_/  \\/  \\/ | ");
+        System.out.println("     \\            / ");
+        System.out.println("      \\__________/ ");
+
+        System.out.println();
     }
+
+
 }
