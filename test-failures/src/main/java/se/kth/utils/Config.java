99c8576ab65f68b5e0b870b15a667cae16f40f33
Leonard Husmann <leonard.husmann@tum.de>
2024-09-16 13:29:13 +0200

    Add DependenciesExtractor with docker client

---
+++ test-failures/src/main/java/se/kth/utils/Config.java
@@ -0,0 +1,28 @@
+package se.kth.utils;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+
+import java.time.Duration;
+
+public class Config {
+    public static DockerClient getDockerClient() {
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .build();
+
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+
+        return DockerClientImpl.getInstance(config, httpClient);
+    }
+}
