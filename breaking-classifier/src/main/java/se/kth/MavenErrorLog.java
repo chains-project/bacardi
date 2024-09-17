bcc9d0a84911363ef0599ac13cdfbbf21801627e
Frank Reyes-Garcia <125555252+frankreyesgarcia@users.noreply.github.com>
2024-09-17 16:43:33 +0200

    Classifier (#3)
    * Setting cli options
    
    * Setting Bacardi core module
    
    * Adding Werror analysis and collecting all information
    
    * Fixing log
    
    * Collection werror information from the client

---
+++ breaking-classifier/src/main/java/se/kth/MavenErrorLog.java
@@ -0,0 +1,43 @@
+package se.kth;
+
+import se.kth.models.ErrorInfo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@lombok.Getter
+@lombok.Setter
+public class MavenErrorLog {
+
+    Map<String, Set<ErrorInfo>> errorInfo;
+
+
+    public MavenErrorLog() {
+        this.errorInfo = new HashMap<>();
+    }
+
+    public void addErrorInfo(String currentPath, ErrorInfo errorInfo) {
+        if (this.errorInfo.containsKey(currentPath)) {
+            for (ErrorInfo error : this.errorInfo.get(currentPath)) {
+                if (error.getClientLinePosition().equals(errorInfo.getClientLinePosition())
+                        && error.getClientFilePath().equals(errorInfo.getClientFilePath())
+                ) {
+                    return;
+                }
+            }
+            this.errorInfo.get(currentPath).add(errorInfo);
+        } else {
+            Set<ErrorInfo> errors = this.errorInfo.computeIfAbsent(currentPath, k -> new HashSet<>());
+            errors.add(errorInfo);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MavenErrorLog{" +
+                "errorInfo=" + errorInfo +
+                '}';
+    }
+}
