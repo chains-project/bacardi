a2e5a86ef67fade6d145f0a228b376d7c3dabd99
Frank Reyes-Garcia <125555252+frankreyesgarcia@users.noreply.github.com>
2024-09-17 16:43:33 +0200

    Classifier (#3)
    * Setting cli options
    
    * Setting Bacardi core module
    
    * Adding Werror analysis and collecting all information
    
    * Fixing log
    
    * Collection werror information from the client

---
+++ breaking-classifier/src/main/java/se/kth/models/WerrorInfo.java
@@ -0,0 +1,60 @@
+package se.kth.models;
+
+
+import se.kth.MavenErrorLog;
+
+import java.io.File;
+import java.util.List;
+
+
+public class WerrorInfo {
+    /**
+     * The error line in the Maven log.
+     */
+    private MavenErrorLog errorLine;
+
+    /**
+     * The list of warning lines in the Maven log.
+     */
+    private MavenErrorLog warningList;
+
+    /**
+     * The list of files where Werror is detected.
+     */
+    private List<File> wErrorFiles;
+
+    /**
+     * A flag indicating whether there is a client configuration problem.
+     */
+    private Boolean isClientConfigProblem;
+
+    private String client;
+
+    /**
+     * Constructs a new WErrorMetadata object with the specified error line, warning list, client configuration problem flag, and list of Werror files.
+     *
+     * @param errorLine             The error line in the Maven log.
+     * @param warningList           The list of warning lines in the Maven log.
+     * @param isClientConfigProblem A flag indicating whether there is a client configuration problem.
+     * @param wErrorFiles           The list of files where Werror is detected.
+     */
+    public WerrorInfo(MavenErrorLog errorLine, MavenErrorLog warningList, Boolean isClientConfigProblem, List<File> wErrorFiles, String client) {
+        super();
+        this.errorLine = errorLine;
+        this.warningList = warningList;
+        this.isClientConfigProblem = isClientConfigProblem;
+        this.wErrorFiles = wErrorFiles;
+        this.client = client;
+    }
+
+    @Override
+    public String toString() {
+        return "WerrorInfo{" +
+                "errorLine=" + errorLine +
+                ", warningList=" + warningList +
+                ", wErrorFiles=" + wErrorFiles +
+                ", isClientConfigProblem=" + isClientConfigProblem +
+                ", client='" + client + '\'' +
+                '}';
+    }
+}
