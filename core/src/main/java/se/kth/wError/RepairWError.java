02fc7051245ba74c2813140c92278e53f46cf7c8
Frank Reyes <frankrg@kth.se>
2024-09-24 14:39:22 +0200

    Setting WError and adding parse for java version incompatibilities

---
+++ core/src/main/java/se/kth/wError/RepairWError.java
@@ -0,0 +1,128 @@
+package se.kth.wError;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class is used to repair the wError configuration file.
+ */
+
+@Getter
+@Setter
+public class RepairWError {
+
+    /*
+     * The tag for the failOnWarning option in the wError configuration file.
+     */
+    private final String failOnWarningTag = "failOnWarning";
+
+    private final Logger log = LoggerFactory.getLogger(RepairWError.class);
+
+
+    /**
+     * Checks if the log file contains a Java version incompatibility error related to wError.
+     *
+     * @param logFilePath the path to the log file to be checked
+     * @return true if the log file contains the Java version incompatibility error pattern, false otherwise
+     * @throws IOException if an I/O error occurs reading the log file
+     */
+    public boolean isWerrorJavaVersionIncompatibilityError(String logFilePath) throws IOException {
+        String javaVersionIncompatibilityPattern = "system modules path not set in conjunction with -source 11";
+        return findPattern(logFilePath, javaVersionIncompatibilityPattern);
+    }
+
+    /**
+     * Searches for a specific pattern in the given log file.
+     *
+     * @param logFilePath the path to the log file to be searched
+     * @param pattern     the regex pattern to search for in the log file
+     * @return true if the pattern is found in the log file, false otherwise
+     * @throws IOException if an I/O error occurs reading the log file
+     */
+    public boolean findPattern(String logFilePath, String pattern) throws IOException {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(logFilePath);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.ISO_8859_1);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line;
+
+            Pattern errorPattern = Pattern.compile(pattern);
+
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = errorPattern.matcher(line);
+                if (matcher.find()) {
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            log.error("An error occurred while reading the log file: {}", e.getMessage());
+            throw e;
+        }
+        return false;
+    }
+
+   /**
+ * Replaces the Java version in the `pom.xml` file with the specified version.
+ * Creates a backup of the original `pom.xml` file before making changes.
+ *
+ * @param pomFilePath the path to the `pom.xml` file
+ * @param javaVersion the Java version to set in the `pom.xml` file
+ * @throws IOException if an I/O error occurs during file operations
+ */
+public void replaceJavaVersion(String pomFilePath, int javaVersion) throws IOException {
+    File pomFile = new File(pomFilePath);
+    File backupPomFile = new File(pomFilePath.replace(".xml", "_backup.xml"));
+
+    // Create a backup copy of the original pom.xml file
+    Files.copy(pomFile.toPath(), backupPomFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    try {
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(pomFile.getAbsolutePath());
+
+        // Normalize the XML structure
+        doc.getDocumentElement().normalize();
+
+        Element root = doc.getDocumentElement();
+        // Set the new Java version for maven.compiler.source and maven.compiler.target
+        root.getElementsByTagName("maven.compiler.source").item(0).setTextContent(String.valueOf(javaVersion));
+        root.getElementsByTagName("maven.compiler.target").item(0).setTextContent(String.valueOf(javaVersion));
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+
+        // Save the modified content back to the original pom.xml file
+        StreamResult result = new StreamResult(pomFile);
+        transformer.transform(source, result);
+
+    } catch (ParserConfigurationException | SAXException | TransformerException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+
+}
