59c2efccaad2b16a2cc63264335a65e0f3e425ca
Leonard Husmann <leonard.husmann@tum.de>
2024-09-27 17:05:42 +0200

    Add CustomExecutionListener with saving TestResult to files

---
+++ test-failures/src/main/java/se/kth/listener/CustomExecutionListener.java
@@ -0,0 +1,35 @@
+package se.kth.listener;
+
+import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class CustomExecutionListener implements TestExecutionListener {
+
+    @Override
+    public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
+        System.out.println(testIdentifier.toString() + ": " + testExecutionResult.toString());
+        System.out.println("-------------------------------------------------");
+        TestResult testResult = new TestResult(testIdentifier.getUniqueId(),
+                testExecutionResult.getThrowable().orElseGet(() -> null));
+        saveTestResult(testResult);
+    }
+
+    private void saveTestResult(TestResult testResult) {
+        String filePath = "/output/test-" + testResult.hashCode() + ".log";
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            objectOutputStream.writeObject(testResult);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private record TestResult(String testIdentifier, Throwable throwable) implements Serializable {
+    }
+
+}
--- test-failures/src/main/java/se/kth/utils/FileUtils.java
+++ test-failures/src/main/java/se/kth/utils/FileUtils.java
@@ -1,6 +1,9 @@
 package se.kth.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,5 +13,14 @@ public class FileUtils {
         return Stream.of(new File(directory).listFiles())
                 .filter(file -> !file.isDirectory())
                 .collect(Collectors.toList());
+    }
+
+    public static Object readFromBinary(String filePath) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+            return objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
