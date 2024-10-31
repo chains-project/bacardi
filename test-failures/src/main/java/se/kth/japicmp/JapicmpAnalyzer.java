163872c66b6f8499cd41a39cec5842040a4e18bf
Leonard Husmann <leonard.husmann@tum.de>
2024-10-31 23:31:24 +0100

    #11 add JapicmpAnalyzer and UpdatedDependencyExtractor

---
+++ test-failures/src/main/java/se/kth/japicmp/JapicmpAnalyzer.java
@@ -0,0 +1,52 @@
+package se.kth.japicmp;
+
+import japicmp.cmp.JApiCmpArchive;
+import japicmp.cmp.JarArchiveComparator;
+import japicmp.cmp.JarArchiveComparatorOptions;
+import japicmp.config.Options;
+import japicmp.model.JApiClass;
+import se.kth.PipelineComponent;
+import se.kth.Util.FileUtils;
+import se.kth.model.BreakingUpdate;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+public class JapicmpAnalyzer extends PipelineComponent {
+
+    private final Path preM2Path;
+    private final Path breakingM2Path;
+
+    public JapicmpAnalyzer(Path preM2Path, Path breakingM2Path) {
+        this.preM2Path = preM2Path;
+        this.breakingM2Path = breakingM2Path;
+    }
+
+    @Override
+    public void execute(BreakingUpdate breakingUpdate) {
+        JarArchiveComparatorOptions comparatorOptions = JarArchiveComparatorOptions.of(getDefaultOptions());
+        JarArchiveComparator jarArchiveComparator = new JarArchiveComparator(comparatorOptions);
+        String commitId = breakingUpdate.breakingCommit;
+
+        File jarFilePre = this.getJarFile(this.preM2Path, commitId);
+        File jarFileBreaking = this.getJarFile(this.breakingM2Path, commitId);
+
+        JApiCmpArchive preArchive = new JApiCmpArchive(jarFilePre, "v1");
+        JApiCmpArchive breakingArchive = new JApiCmpArchive(jarFileBreaking, "v2");
+        List<JApiClass> jApiClasses = jarArchiveComparator.compare(preArchive, breakingArchive);
+        System.out.println(jApiClasses);
+    }
+
+    public File getJarFile(Path basePath, String commitId) {
+        Path projectPath = basePath.resolve(commitId);
+        return FileUtils.getFilesInDirectory(projectPath.toString()).getFirst();
+    }
+
+    public Options getDefaultOptions() {
+        Options defaultOptions = Options.newDefault();
+        defaultOptions.setOutputOnlyModifications(true);
+        defaultOptions.setIgnoreMissingClasses(true);
+        return defaultOptions;
+    }
+}
