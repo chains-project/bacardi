1fca8a2af786683f1ae309a8b2c109f959e199c9
Frank Reyes <frankrg@kth.se>
2025-02-14 13:54:55 +0100

    Updating results names

--- results/results_baseline_anthropic_buggy_line/o3-mini-2025-01-31/402082609522c66a3b790aedafd0570148a7d53f/patches/patch1/original/ChangeSkinCommand.java
+++
@@ -1,9 +0,0 @@
-package com.github.games647.changeskin.sponge.command;
-
-import org.spongepowered.api.command.spec.CommandSpec;
-
-@FunctionalInterface
-public interface ChangeSkinCommand {
-
-    CommandSpec buildSpec();
-}
